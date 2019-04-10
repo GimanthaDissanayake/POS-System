175f0768514afad2df62d92d232e403f03185975
unknown <gimanthad@gmail.com>
2019-04-10 19:25:48 +0530

    commit 1

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 
 <!DOCTYPE html>
 <head>
-    <title>POS SYSTEM</title>
+<link rel="stylesheet" href="CSS/styles.css">
+    <title>RUH SHOP - POS SYSTEM</title>
 </head>
 <body>
-    <p>This is a test</p>
+<div class="login-box">
+    <h1>Sign In</h1>
+    <div class="textbox">
+        <input type="text" placeholder="Username" name="" value="">
+    </div>
+    <div class="textbox">
+        <input type="password" placeholder="Password" name="" value="">
+    </div>
+
+    <input class="btn" type="button" name="" value="Sign In">
+</div>
 </body>
 </html>
