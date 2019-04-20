84c770f5f15f320ccbe886a9396175f84c92de6e
heshancs <36256429+heshancs@users.noreply.github.com>
2019-04-20 09:35:20 +0530

    transection forn contorl done

--- inc/transections.php
+++ inc/transections.php
@@ -3,96 +3,33 @@
 
 <section class="main">
 		<h3 class="transections">Transections</h3>
-		<div class="comtainer-fluid">
+		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-4">
-					<div class="transection_form">
+					  <div class="transection_form">
 
 						
-					<form>
+					    <form>
+                <div class="form-group row">
+                  <label for="inputPassword3" class="col-sm-3 col-form-label">First Name;</label>
+                    <div class="col-sm-9">
+                      <input type="password" class="form-control" placeholder="First Name:" >
+                    </div>
+                </div>
 
                       
-
-
-                      <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Item Name</label>
-                        <div class="col-sm-9">
-                          <input type="password" class="form-control" id="inputPassword3" >
-                        </div>
-                      </div>
-                      
-                      <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Item Price</label>
-                        <div class="col-sm-9">
-                          <input type="email" class="form-control" id="inputPassword3" >
-                        </div>
-                      </div>
-
-                      <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Remaining Quantity</label>
-                       <div class="col-sm-9">
-                          <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                      </div>
-                            
-                      <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Quantity</label>
-                        <div class="col-sm-9">
-                          <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                      </div>
-
-                      <div class="form-group row">
-                      	<div class="col-sm-3">
-                      		
-                      	</div>
-    					<div class="col-sm-9">
-    					  <button type="submit" class="btn btn-primary">Add Item</button>
-    					   <button type="submit" class="btn btn-light">Remove Item</button>
-    					</div>
- 					   </div>
-
- 					   <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Total Bill</label>
-                       <div class="col-sm-9">
-                          <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                      </div>
-
-                      <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Cash</label>
-                       <div class="col-sm-9">
-                          <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                      </div>
-
-                      <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Balance</label>
-                       <div class="col-sm-9">
-                          <input type="password" class="form-control" id="inputPassword3">
-                        </div>
-                      </div>
-
-                       <div class="form-group row">
-                        <div class="col-sm-3">
-                          
-                        </div>
-                          <div class="col-sm-9">
-                              <button type="submit" class="btn btn-primary">Generate  Bill</button>
-                        </div>
-                        </div>
 
 
                             
 
                     
-                    </form>
-                	</div>
+              </form>
+            </div>
 				</div>
 
 				<div class="col-md-8">
 					<div class="cashier_table">
-					<table class="table">
+          <table class="table">
  						 <thead>
  						   <tr>
  						     <th scope="col">#</th>
@@ -118,12 +55,20 @@
  						 
  						 </tbody>
 					</table>
-				</div>
+					
+				  </div>
 					
 				</div>
 			</div>
 		</div>
-	</section>
+  </section>
+  
+  <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-3 col-form-label">Item Name</label>
+                        <div class="col-sm-9">
+                          <input type="password" class="form-control" >
+                        </div>
+                      </div>
 
 
   
