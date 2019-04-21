b26d99c1074fb89daf6d1fa7e71440c7b8efd22c
unknown <gimanthad@gmail.com>
2019-04-21 08:07:01 +0530

    customer data save done

--- JS/main.js
+++ JS/main.js
@@ -46,17 +46,6 @@ itemsRef.on("child_added",snap=>{
 });
 */
 
-//validate customer form
-var customerForm = document.querySelector("#cusForm");
-
-    customerForm.addEventListener('submit',function(event){
-        if(customerForm.checkValidity() === false){
-            event.preventDefault();
-            event.stopPropagation();
-        }
-    customerForm.classList.add('was-validated');
-    })
-
 //validate transaction form
 var transactionForm = document.querySelector("#transForm");
 
@@ -66,4 +55,42 @@ transactionForm.addEventListener('submit',function(event){
             event.stopPropagation();
         }
         transactionForm.classList.add('was-validated');
-    })    
+    })
+
+//validate customer form
+var customerForm = document.querySelector("#cusForm");
+
+    customerForm.addEventListener('submit',function(event){
+        if(customerForm.checkValidity() === false){
+            event.preventDefault();
+            event.stopPropagation();
+            customerForm.classList.add('was-validated');
+        }
+        else{
+            event.preventDefault();
+            var fName=document.getElementById('fName').value;
+            var lName=document.getElementById('lName').value;
+            var gender=document.getElementById('gender').value;
+            var email=document.getElementById('email').value;
+            var address=document.getElementById('address').value;
+            var tp=document.getElementById('telephone').value;
+            saveCustomer(fName,lName,gender,email,address,tp);
+            document.getElementById('cusForm').reset();
+        }
+    })
+
+function saveCustomer(fName,lName,gender,email,address,tp){
+    var cusRef = firebase.database().ref().child("Customer");
+    var newCusRef = cusRef.push();
+    newCusRef.set({
+        firstName: fName,
+        lastName: lName,
+        gender: gender,
+        email: email,
+        address: address,
+        telephone: tp
+    })            
+}  
+
+
+    
