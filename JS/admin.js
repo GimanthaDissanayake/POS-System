bb40646a221c8f5a4e8b0c7e9eb0378f12ec1d23
unknown <gimanthad@gmail.com>
2019-04-27 09:21:08 +0530

    work on manage stock part

--- JS/admin.js
+++ JS/admin.js
@@ -11,3 +11,41 @@ itemsRef.on("child_added",snap=>{
     //load table
     $('#viewStockTbl').append("<tr><td>"+itemCode+"</td><td>"+itemName+"</td><td>"+itemPrice+"</td><td>"+quantity+"</td></tr>");
 });
+
+//load item data
+itemsRef.on("child_added",snap=>{
+    var itemCode = snap.child("itemCode").val();
+
+    $('#itemCode').append("<option>"+itemCode+"</option>");
+    $('#remitemCode').append("<option>"+itemCode+"</option>");
+});
+
+//Add to stock codes
+function loadDataToAdd(){
+    var x = document.getElementById('itemCode').value;
+
+    if(x=='selectID'){
+        document.getElementById('addToForm').reset();
+    }else{
+        itemsRef.orderByChild("itemCode").equalTo(x).on("child_added", function(data) {
+            var newItemName = data.val().itemName;
+            
+            document.getElementById('itemName').value=newItemName;
+         });
+    }   
+}
+
+//remove items code
+function loadDataToRemove(){
+    var x = document.getElementById('remitemCode').value;
+
+    if(x=='selectID'){
+        document.getElementById('remItemForm').reset();
+    }else{
+        itemsRef.orderByChild("itemCode").equalTo(x).on("child_added", function(data) {
+            var newItemName = data.val().itemName;
+            
+            document.getElementById('remitemName').value=newItemName;
+         });
+    }   
+}
